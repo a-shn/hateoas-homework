b9f35a24cf58a0f9aab4012e3cef6c1f62f67d9a
xiu-xiu <sh.il.almaz@gmail.com>
2021-01-08 15:55:36 +0300

    fix

--- src/main/java/ru/itis/repositories/MatriculantsRepository.java
+++ src/main/java/ru/itis/repositories/MatriculantsRepository.java
@@ -1,21 +1,4 @@
 package ru.itis.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@RepositoryRestController
-public class MyProfileController {
-    @Autowired
-    private BachelorsService bachelorsService;
-    @RequestMapping(value = "/bachelors/{bachelor-id}", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<?> getProfileByUserId(@PathVariable("bachelor-id") Long bachelorId) {
-        return ResponseEntity.ok(EntityModel.of(bachelorsService.getBachelor(bachelorId)));
-    }
+public interface MatriculantsRepository {
 }
